842351fd0f4ed96fc52060dc73b99750c86689c5
Ze <79874611+HusseinZtn@users.noreply.github.com>
2022-09-28 16:38:49 +0200

    code formatter

--- src/hze/Main.java
+++ src/hze/Main.java
@@ -11,21 +11,21 @@ public class Main {
 		// declare
 		userinput = new Scanner(System.in);
 		char gender;
-	    double height, weight;
+		double height, weight;
 
 		// Input
 		System.out.print("Please enter your birth gender (m/f): ");
 		gender = userinput.next().charAt(0);
-		
+
 		System.out.print("Please enter your height (cm): ");
 		height = userinput.nextDouble();
-	    
-	    System.out.print("Please enter your weight (kg): ");
-	    weight = userinput.nextDouble();
-	    
-	    // height -> double | weight -> double | gender -> char
-	    System.out.println(Calculator.calculate(height, weight, gender));
-		
+
+		System.out.print("Please enter your weight (kg): ");
+		weight = userinput.nextDouble();
+
+		// height -> double | weight -> double | gender -> char
+		System.out.println(Calculator.calculate(height, weight, gender));
+
 	}
 
 }
